678cc3b65ef8b883a5a9328967be34cbd5ad7449
adityaw20 <adityawalzade203@gmail.com>
2024-03-29 11:13:41 +0530

    dom

---
+++ function.js
@@ -0,0 +1,15 @@
+function learn(course) {
+   console.log(`I am learning ${course}`);
+}
+
+learn("Webdev");
+
+
+
+
+
+function welcomeMsg(name) {
+   console.log(`Hello ${name}!! Welcome to RTC `);
+}
+
+welcomeMsg("Aditya");
